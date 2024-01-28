b1e680faa0abae4c19f3c6e9c81f58cd2c34886e
Francisco-dos-Santos <franciscodossantosmr@gmail.com>
2024-01-28 11:18:27 -0300

    refactorando

--- Users/assets/js/pageCarrinho.js
+++ Users/assets/js/pageCarrinho.js
@@ -1,22 +1,7 @@
-import {openCart, iconCart,handleClicksButtons} from "./workCart.js";
+import {handleClicksButtons} from "./workCart.js";
 import {addNameUser} from"./workheaderLogado.js";
-import { Modal } from "./modal.js";
 
-// events
-// openCart.btncloseCart.onclick=()=>{
-//   openCart.close();
-// }
-// iconCart.onmousemove=()=>{
-//   openCart.open();
-// }
-window.addEventListener("keydown",closeWidthESC)
-function closeWidthESC(event){
-  if(event.key==="Escape"){
-    openCart.close();
-  }
-}
 document.addEventListener("DOMContentLoaded", function(){
   addNameUser();
   handleClicksButtons();
-  Modal.open("","já existe usário com esse email digite outro");
 })
